3e16a15463d4138f1c5bddad2a28fb2b888c00a5
Preti <gsantiago@uolinc.com>
2015-08-25 23:24:30 -0300

    Alterando estrategia de risco/sizing

--- trade_system_strategy.R
+++ trade_system_strategy.R
@@ -44,7 +44,7 @@ TradeSystem$methods(
     
     
     posSize = floor((0.1*.self$accountBalance)/stockValue) #Cada posicao equivale a 10% do saldo
-    stopPosValue = stockValue - ((0.02*.self$accountBalance)/posSize) #Risco de 2%
+    stopPosValue = stockValue - ((0.01*.self$accountBalance)/posSize) #Risco de 1%
     
     return(list(size=posSize, stopPos=stopPosValue))
   },
